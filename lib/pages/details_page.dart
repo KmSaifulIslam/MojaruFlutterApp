94ab80a3f2263c433f25d0b5e11fdeb73356d27c
kmsaiful <kmsaifulislam123@gmail.com>
2023-05-28 10:50:38 +0600

    Details Page Designed.

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,10 +1,289 @@
 import 'package:flutter/material.dart';
 
+import '../utils/colors.dart';
+import '../utils/dimensions.dart';
+import '../widgets/app_icon.dart';
+import '../widgets/big_text.dart';
+import '../widgets/exandable_text_widget.dart';
+import '../widgets/small_text.dart';
+
 class DetailsPage extends StatelessWidget {
   const DetailsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.maxFinite,
+              height: Dimensions.popularFoodImageSize,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                          "assets/images/img.jpg"
+                      )
+                  )
+              ),
+            ),
+          ),
+          Positioned(
+            top: Dimensions.height45,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(icon: Icons.arrow_back_ios),
+                AppIcon(icon: Icons.shopping_cart_outlined),
+              ],
+            ),
+          ),
+          Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: Dimensions.popularFoodImageSize-20,
+              child: Container(
+                  padding: EdgeInsets.only(left: Dimensions.width20,right: Dimensions.width20,top:Dimensions.height20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(Dimensions.radius20),
+                        topLeft: Radius.circular(Dimensions.radius20),
+                      ),
+                      color: Colors.white
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          BigText(text: "Solar System"),
+                          AppIcon(icon: Icons.favorite_outline),
+                        ],
+                      ),
+                      SizedBox(height: Dimensions.height10,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              BigText(text: "For", size: Dimensions.font20,),
+                              SizedBox(width: Dimensions.width10,),
+                              Container(
+                                padding: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20,top: 6, bottom: 6),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: AppColors.contnerColor,
+                                ),
+                                child: Row(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(Icons.person_2_outlined, color: AppColors.mainColor,),
+                                        SizedBox(width: Dimensions.width10,),
+                                        SmallText(text: "6 - 9 Years Old", color: AppColors.paraColor,),
+                                      ],
+                                    ),
+
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: Dimensions.height45),
+                          Container(
+                            padding: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20,top: 6, bottom: 6),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: AppColors.contnerColor,
+                            ),
+                            child: SmallText(text: "Science", color: AppColors.paraColor,),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: Dimensions.height10,),
+                      ExpandableTextWidget(text: "You may think that the earth is a pretty big place, but the Earth isn't even the biggest planet is the solar system."),
+                      SizedBox(height: Dimensions.height20,),
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              BigText(text: "Lessons Preview"),
+                            ],
+                          ),
+                          SizedBox(height: Dimensions.height10,),
+                          SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Row(
+                                children: [
+                                  Stack(
+                                    alignment: Alignment.center,
+                                    children: [
+                                      Opacity(
+                                        opacity:0.5,
+                                        child: Container(
+                                          height: 100,
+                                          width: 200,
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.circular(Dimensions.radius15),
+                                              color: AppColors.mainColor,
+                                              image: DecorationImage(
+                                                fit: BoxFit.cover,
+                                                image: AssetImage(
+                                                    "assets/images/img2.jpg"
+                                                ),
+                                              )
+                                          ),
+                                        ),
+                                      ),
+                                      Icon(Icons.play_circle_outline, color: Colors.white,size: Dimensions.iconSize16*2,),
+                                    ],
+                                  ),
+                                  SizedBox(width: Dimensions.width10,),
+                                  Stack(
+                                    alignment: Alignment.center,
+                                    children: [
+                                      Opacity(
+                                        opacity:0.5,
+                                        child: Container(
+                                          height: 100,
+                                          width: 200,
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.circular(Dimensions.radius15),
+                                              color: AppColors.mainColor,
+                                              image: DecorationImage(
+                                                fit: BoxFit.cover,
+                                                image: AssetImage(
+                                                    "assets/images/img3.jpg"
+                                                ),
+                                              )
+                                          ),
+                                        ),
+                                      ),
+                                      Icon(Icons.play_circle_outline, color: Colors.white,size: Dimensions.iconSize16*2,),
+                                    ],
+                                  ),
+                                  SizedBox(width: Dimensions.width10,),
+                                  Stack(
+                                    alignment: Alignment.center,
+                                    children: [
+                                      Opacity(
+                                        opacity:0.5,
+                                        child: Container(
+                                          height: 100,
+                                          width: 200,
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.circular(Dimensions.radius15),
+                                              color: AppColors.mainColor,
+                                              image: DecorationImage(
+                                                fit: BoxFit.cover,
+                                                image: AssetImage(
+                                                    "assets/images/img2.jpg"
+                                                ),
+                                              )
+                                          ),
+                                        ),
+                                      ),
+                                      Icon(Icons.play_circle_outline, color: Colors.white,size: Dimensions.iconSize16*2,),
+                                    ],
+                                  ),
+                                  SizedBox(width: Dimensions.width10,),
+                                  Stack(
+                                    alignment: Alignment.center,
+                                    children: [
+                                      Opacity(
+                                        opacity:0.5,
+                                        child: Container(
+                                          height: 100,
+                                          width: 200,
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.circular(Dimensions.radius15),
+                                              color: AppColors.mainColor,
+                                              image: DecorationImage(
+                                                fit: BoxFit.cover,
+                                                image: AssetImage(
+                                                    "assets/images/img3.jpg"
+                                                ),
+                                              )
+                                          ),
+                                        ),
+                                      ),
+                                      Icon(Icons.play_circle_outline, color: Colors.white,size: Dimensions.iconSize16*2,),
+                                    ],
+                                  ),
+                                  SizedBox(width: Dimensions.width10,),
+                                ],
+                              )
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+
+              )
+          )
+        ],
+      ),
+      bottomNavigationBar: Container(
+          height: 150,
+          padding: EdgeInsets.only(
+            top: Dimensions.height30,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+          ),
+          decoration: BoxDecoration(
+              color: AppColors.buttomNavColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(Dimensions.radius20*2),
+                topRight: Radius.circular(Dimensions.radius20*2),
+              )
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.access_time_rounded, color: AppColors.singColor,),
+                      SmallText(text: "1 houre 20 minutes",size: 17,color: AppColors.mainBlackColor,),
+                    ],
+                  ),
+                  SmallText(text: "5 Lessons",size: 17,color: AppColors.mainBlackColor,),
+                ],
+              ),
+              SizedBox(height: Dimensions.height15,),
+              Container(
+                padding: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20, top: Dimensions.height20,bottom: Dimensions.height20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: AppColors.mainColor,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        BigText(text: "Start Learning", color: Colors.white,size: Dimensions.font20,),
+                        SizedBox(width: Dimensions.width10,),
+                        Icon(Icons.keyboard_double_arrow_right, color: Colors.white,size:Dimensions.iconSize24,),
+                      ],
+                    ),
+
+                  ],
+                ),
+              ),
+            ],
+          )
+      ),
+    );
   }
 }
