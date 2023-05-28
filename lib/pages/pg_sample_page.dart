17b0ba17143a05b73263dc83f3b4629ca058222f
kmsaiful <kmsaifulislam123@gmail.com>
2023-05-28 07:59:55 +0600

    Project Folder structure and file created

---
+++ lib/pages/pg_sample_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class PgSamplePage extends StatefulWidget {
+  const PgSamplePage({Key? key}) : super(key: key);
+
+  @override
+  State<PgSamplePage> createState() => _PgSamplePageState();
+}
+
+class _PgSamplePageState extends State<PgSamplePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
